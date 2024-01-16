74ffb227781a283ae3b76d64fa003fcaea053358
FinOCE <me@itsf.in>
2024-01-16 15:58:34 +1000

    Create demo project for local testing

---
+++ Filters.Tests.Demo/Triggers/AuthorizeTrigger.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace Semifinals.Filters;
+
+public class AuthorizeTrigger
+{
+    [Authorize]
+    [FunctionName(nameof(AuthorizeTrigger))]
+    public IActionResult Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req)
+    {
+        return new OkObjectResult("Authorize succeeded");
+    }
+}
