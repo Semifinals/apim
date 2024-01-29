4a4dcfce62726f58e97e511daf657856b5dfe7da
FinOCE <me@itsf.in>
2024-01-29 16:49:50 +1000

    Start parsing attributes with CLI project

---
+++ Apim.CLI/Program.cs
@@ -0,0 +1,50 @@
+﻿using Semifinals.Apim.Attributes;
+using Semifinals.Apim.Nodes;
+
+namespace Semifinals.Apim.CLI;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // TODO: Determine assembly path from args
+        Console.WriteLine(string.Join(' ', args));
+        var assembly = Assembly.LoadFrom("");
+
+        var triggers = assembly.GetTypes()
+            // Get all unique policies on each trigger
+            .SelectMany(type => type
+                .GetMethods()
+                .Select(method => (
+                    Name: type.FullName + "." + method.Name,
+                    Method: method,
+                    Policies: method
+                        .GetCustomAttributes(typeof(PolicyAttribute), true)
+                        .Select(attribute => ((PolicyAttribute)attribute).Policy)
+                        .DistinctBy(policy => policy.GetType().Name)
+                        .OrderBy(policy => policy.Priority)
+                        .Reverse())))
+            // Filter out methods that have no policies
+            .Where(trigger => trigger.Policies.Any())
+            // Aggregate policies using <base />
+            .Select(trigger => (
+                trigger.Name,
+                trigger.Method,
+                Policy: trigger.Policies.Reverse().Aggregate((low, high) =>
+                {
+                    high.Inbound.ReplaceNodes(
+                        high.Inbound.Elements().SelectMany((element, i) =>
+                            element.Name == new XBase().Name
+                                ? low.Inbound.Elements()
+                                : new[] { element }));
+
+                    // TODO: Fix above to handle nested <base /> uses
+                    // TODO: Apply this process to all layers, not just inbound
+                    // TODO: Clean up this Aggregate to not need {} (optional)
+
+                    return high;
+                })));
+
+        // TODO: Apply generated trigger policies
+    }
+}
