43d9afbcf7e765b4aa33d17233ac79ff6ee6a758
Fin <me@itsf.in>
2024-02-10 13:12:34 +1000

    Skip validation template for unhandled content types

--- Apim/Templates/ValidateBodyTemplate.cs
+++ Apim/Templates/ValidateBodyTemplate.cs
@@ -8,20 +8,24 @@ public class ValidateBodyTemplate : Template
     public ValidateBodyTemplate(string contentType)
         : base()
     {
-        var type = contentType switch
-        {
-            "application/json" => EXContentValidationType.Json,
-            "application/xml" => EXContentValidationType.Xml,
-            _ => throw new ArgumentException("Unsupported content type")
-        };
-        
-        Add(
-            new XValidateContent(
+        static XValidateContent Validator(
+            EXContentValidationType type,
+            string contentType) => new(
                 unspecifiedContentTypeAction: EAction.Prevent,
                 maxSize: 1024 * 1024 * 2,
                 sizeExceededAction: EAction.Prevent,
                 new XContent(
                     type: contentType,
-                    validateAs: type)));
+                    validateAs: type));
+
+        switch (contentType)
+        {
+            case "application/json":
+                Add(Validator(EXContentValidationType.Json, contentType));
+                break;
+            case "application/xml":
+                Add(Validator(EXContentValidationType.Xml, contentType));
+                break;
+        }
     }
 }
